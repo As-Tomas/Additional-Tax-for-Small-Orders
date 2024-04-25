ad27d33845a16056a692c157aa2598d2412e83b3
Tomas Bance <tomas.bance@gmail.com>
2024-04-25 14:19:05 +0200

    session keep track of whether the notice has been added

--- additional-tax-for-small-orders.php
+++ additional-tax-for-small-orders.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: Additional Tax for Small Orders
- * Plugin URI: http://yourwebsite.com/
+ * Plugin URI: https://github.com/As-Tomas/Additional-Tax-for-Small-Orders
  * Description: Adds an additional tax of 250kr if the cart total is less than 1000kr.
  * Version: 1.0
- * Author: Your Name
- * Author URI: http://yourwebsite.com/
+ * Author: Tomas Bance
+ * Author URI: https://github.com/As-Tomas
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,9 +22,6 @@ function check_cart_total_add_tax() {
         error_log("Cart object not available or not initialized.");
         return;
     }
-
-    static $notice_added = false; // Static variable to track if the notice has been added
-
 
     $cart_total = WC()->cart->subtotal;
     error_log("Cart total: " . $cart_total);
@@ -46,36 +43,22 @@ function check_cart_total_add_tax() {
         error_log("Additional tax applied.");
     }
 
-    if ($cart_total < 1000 && !$notice_added) {
+    // Use WC session to ensure notice is added only once per session
+    if ($cart_total < 1000 && !WC()->session->get('additional_tax_notice_added')) {
         wc_add_notice('An additional tax of 250kr has been added to your cart because the total cart value is less than 1000kr.', 'notice');
-        $notice_added = true; // Set the flag to true after adding the notice
+        WC()->session->set('additional_tax_notice_added', true);
         error_log("Notice added.");
     }
-
-    
 
     error_log("Current fees: " . print_r(WC()->cart->get_fees(), true));
 }
 
 add_action('woocommerce_cart_calculate_fees', 'check_cart_total_add_tax', 10, 1);
 
+// Reset the session variable at the start of each new cart session
+add_action('woocommerce_before_cart', function() {
+    WC()->session->__unset('additional_tax_notice_added');
+});
 
 
 
-
-
-// Check if the fee has already been added
-// $fees = WC()->cart->get_fees();
-// $tax_added = false;
-// foreach ( $fees as $fee ) {
-//     if ( $fee->name === 'Additional Tax' ) {
-//         $tax_added = true;
-//         break;
-//     }
-// }
-
-// // If fee hasn't been added and cart total is less than 1000, add the fee
-// if ( ! $tax_added && WC()->cart->subtotal < 1000 ) {
-//     WC()->cart->add_fee( 'Additional Tax', 250 );
-//     wc_add_notice( 'An additional tax of 250kr has been added to your cart because the total cart value is less than 1000kr.', 'notice' );
-// }
